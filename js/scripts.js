936a5650ec1b0475af9a690172fdc07187765a34
Adela Darmansyah and Andriy Veremyeyev <andriy@>
2020-01-30 09:25:25 -0800

    Create constructors for Board, Space and Player

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,75 @@
 // Business Logic
-function Player(mark) {
-  this.mark = mark;
+function Player(char) {
+  this.char = char;
 }
 
 Player.prototype.mark = function() {
-  return this.mark;
+  return this.char;
 }
 
+function Board (){
+  this.space1 = new Space(1, 1),
+  this.space2 = new Space(1, 2),
+  this.space3 = new Space(1, 3),
+  this.space4 = new Space(2, 1),
+  this.space5 = new Space(2, 2),
+  this.space6 = new Space(2, 3),
+  this.space7 = new Space(3, 1),
+  this.space8 = new Space(3, 2),
+  this.space9 = new Space(3, 3)
+}
+
+Board.prototype.findSpace = function(x, y){
+  for (i=1; i<=9; i++){
+    if (this[`space${i}`].xCoordinate === x && this[`space${i}`].yCoordinate === y){
+      return this[`space${i}`];
+    }
+  }
+}
+
+
+
+
+
+
+function Space (xCoordinate, yCoordinate){
+  this.xCoordinate = xCoordinate;
+  this.yCoordinate = yCoordinate;
+}
+
+Space.prototype.findxCoordinate = function(){
+  return this.xCoordinate;
+}
+
+Space.prototype.findyCoordinate = function(){
+  return this.yCoordinate;
+}
+
+Space.prototype.markWith = function(char){
+  this.mark = char;
+}
+
+Space.prototype.markedBy = function(){
+  return this.mark;
+}
 
 
 // User Interface Logic
 var testPlayer = new Player("X");
 console.log(testPlayer.mark());
+
+var board = new Board();
+console.log(board);
+console.log(board.findSpace(1, 2));
+
+var testSpace = board.findSpace(1, 2);
+
+console.log(testSpace.findxCoordinate());
+console.log(testSpace.findyCoordinate());
+
+testSpace.markWith(testPlayer.mark());
+console.log(testSpace);
+console.log(testSpace.markedBy());
 
 
 $(document).ready(function(){
