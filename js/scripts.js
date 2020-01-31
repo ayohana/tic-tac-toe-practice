49a72790e62ce28a922cc6721e590a525fa40a2f
Adela Darmansyah and Andriy Veremyeyev <andriy@>
2020-01-30 16:03:04 -0800

    Work on 1 click option

--- js/scripts.js
+++ js/scripts.js
@@ -35,11 +35,14 @@ Board.prototype.checkRow = function(){
   var row1 = this.space1.mark + this.space2.mark + this.space3.mark;
   var row2 = this.space4.mark + this.space5.mark + this.space6.mark;
   var row3 = this.space7.mark + this.space8.mark + this.space9.mark;
-  if (row1.charAt(0) === row1.charAt(1) && row1.charAt(1) === row1.charAt(2)){
+  console.log(row1.charAt(0))
+  console.log(row1.charAt(1))
+  console.log(row1.charAt(2))
+  if (row1 === "XXX" || row1 === "OOO"){
     return true;
-  } else if (row2.charAt(0) === row2.charAt(1) && row2.charAt(1) === row2.charAt(2)) {
+  } else if (row2 === "XXX" || row2 === "OOO") {
     return true;
-  } else if (row3.charAt(0) === row3.charAt(1) && row3.charAt(1) === row3.charAt(2)) {
+  } else if (row3 === "XXX" || row3 === "OOO") {
     return true;
   } else {
     return false;
@@ -50,11 +53,11 @@ Board.prototype.checkColumn = function(){
   var col1 = this.space1.mark + this.space4.mark + this.space7.mark;
   var col2 = this.space2.mark + this.space5.mark + this.space8.mark;
   var col3 = this.space3.mark + this.space6.mark + this.space9.mark;
-  if (col1.charAt(0) === col1.charAt(1) && col1.charAt(1) === col1.charAt(2)){
+  if (col1 === "XXX" || col1 === "OOO"){
     return true;
-  } else if (col2.charAt(0) === col2.charAt(1) && col2.charAt(1) === col2.charAt(2)) {
+  } else if (col2 === "XXX" || col2 === "OOO") {
     return true;
-  } else if (col3.charAt(0) === col3.charAt(1) && col3.charAt(1) === col3.charAt(2)) {
+  } else if (col3 === "XXX" || col3 === "OOO") {
     return true;
   } else {
     return false;
@@ -64,19 +67,32 @@ Board.prototype.checkColumn = function(){
 Board.prototype.checkDiagonal = function(){
   var diag1 = this.space1.mark + this.space5.mark + this.space9.mark;
   var diag2 = this.space3.mark + this.space5.mark + this.space7.mark;
-  if (diag1.charAt(0) === diag1.charAt(1) && diag1.charAt(1) === diag1.charAt(2)){
+  if (diag1 === "XXX" || diag1 === "OOO"){
     return true;
-  } else if (diag2.charAt(0) === diag2.charAt(1) && diag2.charAt(1) === diag2.charAt(2)) {
+  } else if (diag2 === "XXX" || diag2 === "OOO") {
     return true;
   } else {
     return false;
   }
 }
 
+Board.prototype.checkWinner = function(curSpace){
+  if (this.checkRow() === true|| this.checkColumn() === true || this.checkDiagonal() === true){
+    $("#player1turn").hide();
+    $("#player2turn").hide();
+    if (curSpace.markedBy() === "X"){
+      $("#player1wins").show();
+    } else {
+      $("#player2wins").show();      
+    }
+  }
+}
+
 // Space Constructor --------------------------
 function Space (xCoordinate, yCoordinate){
-  this.xCoordinate = xCoordinate;
-  this.yCoordinate = yCoordinate;
+  this.xCoordinate = xCoordinate,
+  this.yCoordinate = yCoordinate,
+  this.mark = ""
 }
 
 Space.prototype.findxCoordinate = function(){
@@ -102,17 +118,29 @@ function Game() {
   this.board = new Board();
 }
 
-Game.prototype.changeStatus = function(){
+Game.prototype.changeStatus = function(cur){
   if (this.player1.status){
     this.player1.status = false;
     this.player2.status = true;
+    cur.text("X");
+    $("#player1turn").hide();
+    $("#player2turn").show();
+    return this.player1;
   } else {
     this.player1.status = true;
     this.player2.status = false;
+    cur.text("O");
+    $("#player1turn").show();
+    $("#player2turn").hide();
+    return this.player2;
   }
-  console.log(this.player1)
-  console.log(this.player2)
 }
+
+// function foo() {
+//   // do things, then
+//   removeEventListener('click', "div#space1"));
+// }
+
 
 var game = new Game();
 
@@ -120,46 +148,86 @@ var game = new Game();
 // User Interface Logic
 
 
-//     testSpace1.markWith(testPlayer2.mark());
-//   ($("div#space2").on("click", function()){
-//   });
-//   $("div#space3").on("click", function(){
-//   });
-//   $("div#space4").on("click", function(){
-//     console.log("hello");
-//   });
-//   $("div#space5").on("click", function(){
-//     console.log("hello");
-//   });
-//   $("div#space6").on("click", function(){
-//     console.log("hello");
-//   });
-//   $("div#space7").on("click", function(){
-//     console.log("hello");
-//   });
-//   $("div#space8").on("click", function(){
-//     console.log("hello");
-//   });
-//   $("div#space9").on("click", function(){
-//     console.log("hello");
-//   });
-// }
-
-
-
 $(document).ready(function(){
   $("div#space1").click(function(event){
     event.preventDefault;
-    game.changeStatus();
-    game.board.space1.markWith(game.player1.mark())
-    console.log(game.board.space1);
+    var currentSpace = $("div#space1");
+    var currentPlayer = game.changeStatus(currentSpace);
+    game.board.space1.markWith(currentPlayer.mark());
+    game.board.checkWinner(game.board.space1);
+    currentSpace.off('click');
   });
-})
 
-// Further Exploration:
-// Make players able to choose if they want to play X or O
-// Game.prototype.makeChoice{
-//   player1
-//   if player.choiceX === 
-//   player = "X"
-//   e
+  $("div#space2").click(function(event){
+    event.preventDefault;
+    var currentSpace = $("div#space2");
+    var currentPlayer = game.changeStatus(currentSpace);
+    game.board.space2.markWith(currentPlayer.mark());
+    game.board.checkWinner(game.board.space2);
+    currentSpace.off('click');
+  });
+
+  $("div#space3").click(function(event){
+    event.preventDefault;
+    var currentSpace = $("div#space3");
+    var currentPlayer = game.changeStatus(currentSpace);
+    game.board.space3.markWith(currentPlayer.mark());
+    game.board.checkWinner(game.board.space3);
+    currentSpace.off('click');
+  });
+
+  $("div#space4").click(function(event){
+    event.preventDefault;
+    var currentSpace = $("div#space4");
+    var currentPlayer = game.changeStatus(currentSpace);
+    game.board.space4.markWith(currentPlayer.mark());
+    game.board.checkWinner(game.board.space4);
+    currentSpace.off('click');
+  });
+
+  $("div#space5").click(function(event){
+    event.preventDefault;
+    var currentSpace = $("div#space5");
+    var currentPlayer = game.changeStatus(currentSpace);
+    game.board.space5.markWith(currentPlayer.mark());
+    game.board.checkWinner(game.board.space5);
+    currentSpace.off('click');
+  });
+
+  $("div#space6").click(function(event){
+    event.preventDefault;
+    var currentSpace = $("div#space6");
+    var currentPlayer = game.changeStatus(currentSpace);
+    game.board.space6.markWith(currentPlayer.mark());
+    game.board.checkWinner(game.board.space6);
+    currentSpace.off('click');
+  });
+
+  $("div#space7").click(function(event){
+    event.preventDefault;
+    var currentSpace = $("div#space7");
+    var currentPlayer = game.changeStatus(currentSpace);
+    game.board.space7.markWith(currentPlayer.mark());
+    game.board.checkWinner(game.board.space7);
+    currentSpace.off('click');
+  });
+
+  $("div#space8").click(function(event){
+    event.preventDefault;
+    var currentSpace = $("div#space8");
+    var currentPlayer = game.changeStatus(currentSpace);
+    game.board.space8.markWith(currentPlayer.mark());
+    game.board.checkWinner(game.board.space8);
+    currentSpace.off('click');
+  });
+
+  $("div#space9").click(function(event){
+    event.preventDefault;
+    var currentSpace = $("div#space9");
+    var currentPlayer = game.changeStatus(currentSpace);
+    game.board.space9.markWith(currentPlayer.mark());
+    game.board.checkWinner(game.board.space9);
+    currentSpace.off('click');
+  });
+
+})
